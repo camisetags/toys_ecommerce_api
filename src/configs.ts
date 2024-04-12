c4dac82e8ca5d5d604bc3e4ab455d3ceb3c822af
Gabriel Seixas <salesseixasgabriel@gmail.com>
2024-04-12 19:07:07 -0300

    fixed env validation

--- src/configs.ts
+++ src/configs.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const configsSchema = z.object({
-  port: z.number()
+  PORT: z.string().regex(/^\d+$/, 'A string deve conter apenas números.').default('3000')
 })
 
 export type Configs = z.infer<typeof configsSchema>
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { configs } from './configs'
 import server from './infra/server'
 
-const app = server.listen(configs.port)
+const app = server.listen(configs.PORT)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
