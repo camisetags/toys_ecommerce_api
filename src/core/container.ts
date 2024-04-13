85dd5a209d7bb9e8f24865a14c58192ad8d1cfd6
Gabriel Seixas <salesseixasgabriel@gmail.com>
2024-04-13 04:20:00 -0300

    Refactor container class in container.ts

--- src/core/container.ts
+++ src/core/container.ts
@@ -1,11 +1,9 @@
 import { db } from './infra/database'
 import { type UserReposisotry } from '../domain/users/repositories'
 import { UserDrizzleReposisotry } from '../domain/users/infra/repositories'
-import { type DrizzleDB } from './infra'
 
 class Container {
-  public dbConnection = (): DrizzleDB => db
-  public getUsersRepository = (): UserReposisotry => new UserDrizzleReposisotry(this.dbConnection())
+  public getUsersRepository = (): UserReposisotry => new UserDrizzleReposisotry(db)
 }
 
 export default new Container()
